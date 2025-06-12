404982b6864dfe66b5d4795e77a42f5c119f63a4
Lucas Paim <lucaspaim924@gmail.com>
2025-06-12 10:01:10 -0300

    Aula 1 - ReactJS, primeiros passos

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+//usaremos essa área para importar coisas
+
+function App() {
+  const nome = "Lucas";
+  const novoNome = nome.toUpperCase(); //deixa a letra maiúscula
+  function soma(a, b) {
+    return a + b;
+  }
+
+  const url = "https://picsum.photos/300";
+  
+  return (
+    //Aqui, precisa usar className, sempre!
+    <div className="App">    
+      <h1>Alterando o JSX</h1>
+      <p>Olá, {nome}</p>
+      <p>Olá, {novoNome}</p>
+      <p>Soma: {soma(1, 2)}</p>
+      <img src={url} alt="Minha imagem" />
+    </div>
+  );
+}
+
+export default App;
